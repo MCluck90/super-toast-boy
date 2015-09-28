6c590db48f0f390ce50ac6df5d4c1f275b84574e
James <rockking1379@gmail.com>
2015-09-28 14:24:03 -0600

    started work on jelly powerup
    added sprite sheet for jelly powerup. its animated...working on getting
    it to work with collision

---
+++ Assets/Scripts/jelly_jar_collision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jelly_jar_collision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
