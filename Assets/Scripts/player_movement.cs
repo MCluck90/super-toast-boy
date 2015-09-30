5d316719313dec98c8e7defcba510e7b06871874
Alfredo <alfredocabrera@live.com>
2015-09-29 18:19:49 -0600

    Fixed player_movement. Added jump feature to player_movement script. I also added new variable that allows to controlled jumpheight and I disabled the "turn" player feature.

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -6,37 +6,51 @@ public class player_movement : MonoBehaviour {
 	public float moveSpeed;
 	private Vector3 input;
 	public Rigidbody player;
-	private Vector3 spawn;
 
 	
 	// Use this for initialization
 	void Start () {
-		spawn = transform.position;
 		player = GetComponent<Rigidbody> ();
 		
 	}
 	
 	public float turnSpeed = 50f;
+	public float jumpheight = 250f;
+	bool grounded = false;
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void FixedUpdate () 
+		{
 		
-		input = new Vector3 (Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+			input = new Vector3 (Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
 		
-		if (Input.GetKey ("w"))
-			player.AddRelativeForce (input * moveSpeed);
-		if (Input.GetKey ("d"))
-			transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
-		//player.AddRelativeForce (input * moveSpeed);
-		if (Input.GetKey ("a"))
-			transform.Rotate (Vector3.up, -turnSpeed * Time.deltaTime);
-		//player.AddRelativeForce (input * moveSpeed);
-		if (Input.GetKey ("s"))
-			player.AddRelativeForce (input * moveSpeed);
-		
-	}
+			if (Input.GetKey ("w"))
+				player.AddRelativeForce (input * moveSpeed);
+			if (Input.GetKey ("d"))
+				player.AddRelativeForce (input * moveSpeed);
+			if (Input.GetKey ("a"))
+				player.AddRelativeForce (input * moveSpeed);
+			if (Input.GetKey ("s"))
+				player.AddRelativeForce (input * moveSpeed);
+			if (Input.GetButton ("Jump"))
+				Jump();
+			//player.AddForce(Vector3.up * jumpheight);
 
-	
+		}
+
+		void OnCollisionEnter(Collision collision) 
+			{
+				grounded = true;
+			}
+
+	    void Jump() 
+		{
+			if (grounded == true) 
+				{
+					GetComponent<Rigidbody>().AddForce (Vector3.up * jumpheight);
+					grounded = false;
+				}
+		}
 	
 	
 }
