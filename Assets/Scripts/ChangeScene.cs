05534cca402b7aaa7fb633314591c636a721ce43
thewywy <thewywy@gmail.com>
2015-10-22 00:37:09 -0600

    fix

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -1,9 +1,11 @@
-﻿//using UnityEngine;
-//using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 
-//public class ChangeScene : MonoBehaviour {
-	
-//	public void ChangeToScene (int i) {
-//		Application.LoadLevel ("Level_" + i);
-//	}
-//}
+public class ChangeScene : MonoBehaviour
+{
+
+    public void ChangeToScene(int i)
+    {
+        Application.LoadLevel("Level_" + i);
+    }
+}
