0d56eb9e167c417e1d0861550eaf220afe9ecb45
Mike C <MCluck90@gmail.com>
2015-11-12 18:31:38 -0700

    Player can now progress through main menu by hitting jump

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -3,6 +3,16 @@ using System.Collections;
 
 public class ChangeScene : MonoBehaviour
 {
+	public void Update() {
+		if (Input.GetButtonDown("Jump")) {
+			int nextLevel = Application.loadedLevel + 1;
+			if (nextLevel >= Application.levelCount) {
+				Application.Quit();
+			} else {
+				Application.LoadLevel(Application.loadedLevel + 1);
+			}
+		}
+	}
 
     public void ChangeToScene(int i)
     {
